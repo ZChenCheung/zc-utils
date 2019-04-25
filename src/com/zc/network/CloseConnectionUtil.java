dcb943607f18bbd7e6689795f810c112fd435aa1
ZChenCheung <526583280@qq.com>
2019-04-25 19:28:06 +0800

    网络编程常用工具

--- src/com/zc/network/CloseConnectionUtil.java
+++ src/com/zc/network/CloseConnectionUtil.java
@@ -6,57 +6,30 @@ import java.io.OutputStream;
 import java.net.Socket;
 
 public class CloseConnectionUtil {
+
     private CloseConnectionUtil(){}
 
-    /**
-     * 关闭输出信道
-     * @param outputStream
-     */
     public static void closeOutputStream(OutputStream outputStream) {
         closeSocketAndCommunication(null, null, outputStream);
     }
 
-    /**
-     * 关闭输入信道
-     * @param inputStream
-     */
     public static void closeInputStream(InputStream inputStream) {
         closeSocketAndCommunication(null, inputStream, null);
     }
 
-    /**
-     * 关闭Socket
-     * @param socket
-     */
     public static void closeSocket(Socket socket) {
         closeSocketAndCommunication(socket, null, null);
     }
 
-    /**
-     * 关闭Socket，并且只关闭输入信道
-     * @param socket
-     * @param inputStream
-     */
     public static void closeSocketAndInputStream(Socket socket, InputStream inputStream) {
         closeSocketAndCommunication(socket, inputStream, null);
     }
 
-    /**
-     * 关闭Socket，并且只关闭输出信道
-     * @param socket
-     * @param outputStream
-     */
     public static void closeSocketAndOutputStream(Socket socket, OutputStream outputStream) {
         closeSocketAndCommunication(socket, null, outputStream);
     }
 
-    /**
-     * 关闭Socket，并且关闭双工通信信道
-     * @param socket
-     * @param inputStream
-     * @param outputStream
-     */
-    public static void closeSocketAndCommunication(Socket socket, 
+    public static void closeSocketAndCommunication(Socket socket,
     		InputStream inputStream, OutputStream outputStream) {
         if (inputStream != null) {
             try {
